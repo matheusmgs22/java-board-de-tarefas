7f7977eccb0c0cea656b4e356e68f3545ee599d3
matheusmgs22 <matheusmgs17@gmail.com>
2025-02-24 21:51:33 -0300

    Setup Inicial do Projeto

--- app/src/main/java/br/com/dio/persistence/migration/MigrationStrategy.java
+++ app/src/main/java/br/com/dio/persistence/migration/MigrationStrategy.java
@@ -1,26 +1,41 @@
 package br.com.dio.persistence.migration;
 
+import liquibase.Liquibase;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.ClassLoaderResourceAccessor;
+import lombok.AllArgsConstructor;
+
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.sql.Connection;
+import java.sql.SQLException;
 
-import lombok.AllArgsConstructor;
-import lombok.var;
+import static br.com.dio.persistence.config.ConnectionConfig.getConnection;
 
 @AllArgsConstructor
 public class MigrationStrategy {
 
     private final Connection connection;
 
-    private void executeMigration() {
-
+    public void executeMigration() {
         var originalOut = System.out;
         var originalErr = System.err;
-
-        try {
-            try (var fos = new FileOutputStream("liquibase.log")) {
-
+        try (var fos = new FileOutputStream("liquibase.log")) {
+            System.setOut(new PrintStream(fos));
+            System.setErr(new PrintStream(fos));
+            try (
+                    var connection = getConnection();
+                    var jdbcConnection = new JdbcConnection(connection);) {
+                var liquibase = new Liquibase(
+                        "/db/changelog/db.changelog-master.yml",
+                        new ClassLoaderResourceAccessor(),
+                        jdbcConnection);
+                liquibase.update();
+            } catch (SQLException | LiquibaseException e) {
+                e.printStackTrace();
+                System.setErr(originalErr);
             }
         } catch (IOException ex) {
             ex.printStackTrace();
@@ -29,6 +44,5 @@ public class MigrationStrategy {
             System.setErr(originalErr);
         }
     }
-}
 
-//15:34
+}
